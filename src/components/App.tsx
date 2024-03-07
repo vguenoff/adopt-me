178f9052d9970965d754b3bf789d9020c7434237
Vasil Genov <vasil.genov@psinteractive.io>
2024-03-07 13:43:12 +0200

    feat: vite setup 2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,20 @@
-import styles from './App.module.css'
+import { createElement } from 'react'
+
+type PetProps = { name: string; animal: string; breed: string }
+
+function Pet({ name, animal, breed }: PetProps) {
+  return createElement('div', {}, [
+    createElement('h1', {}, name),
+    createElement('h2', {}, animal),
+    createElement('h2', {}, breed),
+  ])
+}
 
 export default function App() {
-  return (
-    <div className={styles.root}>
-      <p>adopt me 123</p>
-    </div>
-  )
+  return createElement('div', {}, [
+    createElement('h1', {}, 'Adopt Me!'),
+    createElement(Pet, { name: 'Lori', animal: 'Dog', breed: 'Mixed' }),
+    createElement(Pet, { name: 'Tequila', animal: 'Dog', breed: 'Pitbull' }),
+    createElement(Pet, { name: 'Lusi', animal: 'Dog', breed: 'Mixed' }),
+  ])
 }
