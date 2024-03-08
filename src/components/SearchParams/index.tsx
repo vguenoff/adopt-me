ccefe680e0b35a48c311a841fa9b4dead8903dd6
Vasil Genov <vasil.genov@psinteractive.io>
2024-03-08 20:35:05 +0200

    feat: searchparams wip

--- src/components/SearchParams/index.tsx
+++ src/components/SearchParams/index.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
 import styles from './index.module.css'
 
 export default function SearchParams() {
-  const location = 'Seattle, WA'
+  const [location, setLocation] = useState('')
 
   return (
     <div className={styles.searchParams}>
       <form>
         <label htmlFor="location">Location:</label>
-        <input id="location" value={location} placeholder="Location" />
+        <input
+          id="location"
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+          placeholder="Seattle, WA"
+        />
         <button>Submit</button>
       </form>
     </div>
