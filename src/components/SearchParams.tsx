8ac7ab1f906d76cf2b69464b6f9c6887970e4086
Vasil Genov <vasil.genov@psinteractive.io>
2024-03-19 18:02:38 +0200

    feat: select fin

--- src/components/SearchParams.tsx
+++ src/components/SearchParams.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import styles from './SearchParams.module.css'
 
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
+const BREEDS: string[] = []
+
 export default function SearchParams() {
   const [location, setLocation] = useState('')
+  const [animal, setAnimal] = useState('')
+  const [breed, setBreed] = useState('')
 
   return (
     <div className={styles.searchParams}>
@@ -14,6 +19,31 @@ export default function SearchParams() {
           onChange={e => setLocation(e.target.value)}
           placeholder="Seattle, WA"
         />
+        <label htmlFor="animal">Animal: </label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={e => {
+            setAnimal(e.target.value)
+            setBreed('')
+          }}
+        >
+          <option />
+          {ANIMALS.map(animal => (
+            <option key={animal}>{animal}</option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed: </label>
+        <select
+          id="breed"
+          value={breed}
+          onChange={e => setBreed(e.target.value)}
+          disabled={!BREEDS.length}
+        >
+          {BREEDS.map(breed => (
+            <option key={breed}>{breed}</option>
+          ))}
+        </select>
         <button>Submit</button>
       </form>
     </div>
