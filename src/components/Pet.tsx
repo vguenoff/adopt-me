b0fe76147f6d96ef2d597c14b9b913406d983544
Vasil Genov <vasil.genov@psinteractive.io>
2024-03-20 12:43:22 +0200

    feat: styles

--- src/components/Pet.tsx
+++ src/components/Pet.tsx
@@ -1,3 +1,5 @@
+import styles from './Pet.module.css'
+
 export type PetResponse = {
   animal: string
   breed: string
@@ -9,16 +11,24 @@ export type PetResponse = {
   state: string
 }
 
-export type PetProps = Omit<PetResponse, 'city' | 'state' | 'id'> & {
+export type PetProps = Omit<PetResponse, 'description' | 'city' | 'state'> & {
   location: string
 }
 
-export default function Pet({ name, animal, breed }: PetProps) {
+export default function Pet({ name, animal, breed, images, location, id }: PetProps) {
+  const hero = images.length ? images[0] : 'https://pets-images.dev-apis.com/pets/none.jpg'
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className={styles.pet}>
+      <section>
+        <img src={hero} alt={name} />
+      </section>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
   )
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -4,18 +4,14 @@ import type { PetResponse } from '@/components/Pet'
 
 export default function Results({ pets }: { pets: PetResponse[] }) {
   return (
-    <>
+    <section className="results">
       {!pets.length ? (
         <h1>No pets found.</h1>
       ) : (
-        pets.map(({ id, name, animal, breed, images, description, city, state }) => (
-          <Pet
-            key={id}
-            location={`${city}, ${state} `}
-            {...{ name, animal, breed, images, description }}
-          />
+        pets.map(({ id, name, animal, breed, images, city, state }) => (
+          <Pet key={id} location={`${city}, ${state} `} {...{ name, animal, breed, images, id }} />
         ))
       )}
-    </>
+    </section>
   )
 }
